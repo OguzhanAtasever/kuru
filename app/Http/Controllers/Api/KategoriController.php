327d370b6c9d64b291015069a211bee1fa5726d6
RenoGunray <pike.msonda@gmail.com>
2019-06-26 11:53:43 +0300

    added repository pattern to api controller

--- app/Http/Controllers/Api/KategoriController.php
+++ app/Http/Controllers/Api/KategoriController.php
@@ -5,12 +5,19 @@ namespace App\Http\Controllers\Api;
 use App\Models\Kategori;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Interfaces\KategoriRepositoryInterface;
 
 class KategoriController extends Controller
 {
+    private  $kategoriRepository;
+
+    public function __construct(KategoriRepositoryInterface $kategoriRepository) {
+        $this->kategoriRepository = $kategoriRepository;
+
+    }
+
     public function kategoriler()
     {
-        $kategoriler = Kategori::all();
-        return response()->json($kategoriler);
+        return response()->json($this->kategoriRepository->hepsi());
     }
 }
